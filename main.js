b971c4e8243c55177c8fdf41c2af0dba870dfe68
qbRobert <birlan_robert@yahoo.com>
2023-06-20 23:16:48 +0100

    New files

---
+++ main.js
@@ -0,0 +1,48 @@
+// let today = new Date(); //create a new date object
+// let hourNow = today.getHours(); // find the current hour
+// let greeting = "";
+
+//display the appropriate greeting based on the current time
+// if(hourNow >= 18) {
+//     greeting = "Good evening !"
+// } else if (hourNow >= 12) {
+//     greeting = "Good afternoon !"
+// } else if (hourNow >= 24) {
+//     greeting = "Good morning !"
+// } else {
+//     greeting = "Welcome !";
+// };
+// alert(greeting);
+
+/*let inStock = true;
+let shipping = false;
+
+let elStock = document.getElementById('stock');
+elStock.textContent = inStock ? 'Available' : 'Out of stock';
+
+let elShip =  document.getElementById('shipping');
+elShip.textContent = shipping ? 'Shipping: Yes' : 'No';
+*/
+// const dragos = {
+//     firstName: "Dragos",
+//     gender: "male",
+//     nextObjectReferenceCode: "6511010011410110197"
+//   };
+  
+//   function getCharCodes(obj) {
+//     const charCodes = {};
+  
+//     for (let key in obj) {
+//       if (typeof obj[key] === "string") {
+//         charCodes[key] = [];
+  
+//         for (let i = 0; i < obj[key].length; i++) {
+//           charCodes[key].push(obj[key].charCodeAt(i));
+//         }
+//       }
+//     }
+  
+//     return charCodes;
+//   }
+  
+//   console.log(getCharCodes(dragos));
